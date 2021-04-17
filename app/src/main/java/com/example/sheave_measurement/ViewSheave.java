a1a90294f23a9700708a508789863f102bef0f07
aauxbjd <aauxbjd@gmail.com>
2021-04-17 15:44:53 -0500

    minor fixes to the custom adapter

--- app/src/main/java/com/example/sheave_measurement/ViewSheave.java
+++ app/src/main/java/com/example/sheave_measurement/ViewSheave.java
@@ -1,14 +1,61 @@
 package com.example.sheave_measurement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ViewSheave extends AppCompatActivity {
+    private ListView lv;
+    ArrayList<SheaveMeasurements> list = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_sheave);
+        final MeasurementListAdapter adapter = new MeasurementListAdapter(this, R.layout.adapter_view_layout,list);
+
+        lv = findViewById(R.id.listView);
+        lv.setAdapter(adapter);
+
+        DatabaseReference df = FirebaseDatabase.getInstance().getReference().child("Measurements");
+
+        df.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                list.clear();
+
+                Iterable<DataSnapshot> measurements = dataSnapshot.getChildren();
+
+                for(DataSnapshot child: measurements){
+                    SheaveMeasurements sm = child.getValue(SheaveMeasurements.class);
+                    list.add(sm);
+                    Log.d("Data\n",sm.toString());
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled( DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
     }
 }
