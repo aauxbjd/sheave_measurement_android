68ea0ae791092937d26e085c3a06311c87159827
aauxbjd <aauxbjd@gmail.com>
2021-04-23 19:58:19 -0500

    implemented new activity to view data from the list

--- app/src/main/java/com/example/sheave_measurement/ViewSheave.java
+++ app/src/main/java/com/example/sheave_measurement/ViewSheave.java
@@ -3,10 +3,14 @@ package com.example.sheave_measurement;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -51,6 +55,17 @@ public class ViewSheave extends AppCompatActivity {
             @Override
             public void onCancelled( DatabaseError databaseError) {
 
+            }
+        });
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SheaveMeasurements sv = (SheaveMeasurements) lv.getItemAtPosition(position);
+                Intent intent = new Intent(ViewSheave.this,SingleListItem.class);
+                intent.putExtra("data", sv );
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(), sv.toString(), Toast.LENGTH_LONG).show();
             }
         });
 
